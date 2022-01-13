22cb10d7635c9b220c02a209d96e6c4d73f32f45
Angeliau <3052952191@qq.com>
2022-01-13 10:57:46 +0800

    创建angular带路由项目

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+//配置路由的模块
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
